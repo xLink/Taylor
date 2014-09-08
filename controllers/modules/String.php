79a2fc173812a960ac9a7f2f0fda90e8acd118c2
xLink <xLink@cybershade.org>
2014-09-08 20:15:17 +0100

    added some help to some of the functions

--- controllers/modules/String.php
+++ controllers/modules/String.php
@@ -6,31 +6,58 @@ use Cysha\Modules\Taylor\Helpers\Irc as Irc;
 $trigger = \Config::get('taylor::bot.command_trigger', '>');
 
 Command::register($trigger.'base64_decode', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <b64_string>');
+    }
+
     $text = base64_decode($command->text);
     return Message::privmsg($command->message->channel(), color($text));
 });
 
 Command::register($trigger.'base64_encode', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <text>');
+    }
+
     $text = base64_encode($command->text);
     return Message::privmsg($command->message->channel(), color($text));
 });
 
 Command::register($trigger.'sha1', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <text>');
+    }
+
     $text = sha1($command->text);
     return Message::privmsg($command->message->channel(), color($text));
 });
 
 Command::register($trigger.'md5', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <text>');
+    }
+
     $text = md5($command->text);
     return Message::privmsg($command->message->channel(), color($text));
 });
 
 Command::register($trigger.'strlen', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <text>');
+    }
+
     $text = strlen($command->text);
     return Message::privmsg($command->message->channel(), sprintf('Given string is %d characters in length', $text));
 });
 
 Command::register($trigger.'hex2dec', function (Command $command) {
+    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
+        return Message::privmsg($command->message->channel(), 'Usage: <hex>');
+    }
+    if (empty($command->text)) {
+        return Message::privmsg($command->message->channel(), 'You need give me something to turn into RGB.');
+    }
+
     $color = str_replace('#', '', $command->params[0]);
     $ret = array(
         'r' => hexdec(substr($color, 0, 2)),
@@ -44,11 +71,11 @@ Command::register($trigger.'hex2dec', function (Command $command) {
 });
 
 Command::register($trigger.'str2hex', function (Command $command) {
-    if (empty($command->text)) {
-        return Message::privmsg($command->message->channel(), 'You need to give me something to hex.');
-    }
     if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
         return Message::privmsg($command->message->channel(), 'Usage: <hex>');
+    }
+    if (empty($command->text)) {
+        return Message::privmsg($command->message->channel(), 'You need to give me something to hex.');
     }
 
     $split = str_split($command->text);
