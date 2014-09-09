9642976ef875653e5a72e225c5fcad7166c86174
xLink <xLink@cybershade.org>
2014-09-09 13:53:09 +0100

    replaced a few well placed functions :P

--- controllers/modules/String.php
+++ controllers/modules/String.php
@@ -5,51 +5,6 @@ use Cysha\Modules\Taylor\Helpers\Irc as Irc;
 
 $trigger = \Config::get('taylor::bot.command_trigger', '>');
 
-Command::register($trigger.'base64_decode', function (Command $command) {
-    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
-        return Message::privmsg($command->message->channel(), 'Usage: <b64_string>');
-    }
-
-    $text = base64_decode($command->text);
-    return Message::privmsg($command->message->channel(), color($text));
-});
-
-Command::register($trigger.'base64_encode', function (Command $command) {
-    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
-        return Message::privmsg($command->message->channel(), 'Usage: <text>');
-    }
-
-    $text = base64_encode($command->text);
-    return Message::privmsg($command->message->channel(), color($text));
-});
-
-Command::register($trigger.'sha1', function (Command $command) {
-    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
-        return Message::privmsg($command->message->channel(), 'Usage: <text>');
-    }
-
-    $text = sha1($command->text);
-    return Message::privmsg($command->message->channel(), color($text));
-});
-
-Command::register($trigger.'md5', function (Command $command) {
-    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
-        return Message::privmsg($command->message->channel(), 'Usage: <text>');
-    }
-
-    $text = md5($command->text);
-    return Message::privmsg($command->message->channel(), color($text));
-});
-
-Command::register($trigger.'strlen', function (Command $command) {
-    if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
-        return Message::privmsg($command->message->channel(), 'Usage: <text>');
-    }
-
-    $text = strlen($command->text);
-    return Message::privmsg($command->message->channel(), sprintf('Given string is %d characters in length', $text));
-});
-
 Command::register($trigger.'hex2dec', function (Command $command) {
     if (empty($command->params[0]) || substr($command->params[0], 0, 1) == '?') {
         return Message::privmsg($command->message->channel(), 'Usage: <hex>');
@@ -84,4 +39,46 @@ Command::register($trigger.'str2hex', function (Command $command) {
         $text .= '\x' . bin2hex($char);
     }
     return Message::privmsg($command->message->channel(), color($text));
+});
+
+Message::listen('privmsg', function ($message) {
+    $whitelist = ['md5', 'sha1', 'strlen', 'base64_decode', 'base64_encode', 'str_replace'];
+
+    if (count($message->params) == 2) {
+        list($channel, $params) = $message->params;
+    }
+
+    $params = explode(' ', $params);
+    $command = strtolower(array_shift($params));
+
+    if (substr($command, 0, 1) != '>') {
+        return;
+    }
+    $command = substr($command, 1);
+
+    if (!in_array($command, $whitelist)) {
+        return;
+    }
+
+    // make sure it's only checking PHPs functions and not self defined
+    $functions = array_get(get_defined_functions(), 'internal');
+    if (!in_array($command, $functions)) {
+        return Message::privmsg($channel, color($command.' isnt a PHP Function'));
+    }
+
+    $return = call_user_func_array($command, $params);
+    if ($return === false) {
+        return Message::privmsg($channel, color(sprintf(
+            'PHP> %s(\'%s\') // invalid',
+            $command,
+            implode('\', \'', $params)
+        )));
+    }
+
+    return Message::privmsg($channel, color(sprintf(
+        'PHP> %s(\'%s\') // %s',
+        $command,
+        implode('\', \'', $params),
+        color($return, 'green')
+    )));
 });
