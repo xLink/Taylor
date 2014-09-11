71c5d3af093c5894e819f8c27d2e1e24d01e33c9
xLink <xLink@cybershade.org>
2014-09-11 15:00:51 +0100

    moved whitelist out to a config file

---
+++ config/php.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'md5', 'sha1', 'strlen', 'base64_decode', 'base64_encode', 'str_replace', 'number_format',
+    'ucwords', 'substr', 'strrev', 'str_shuffle', 'str_split', 'explode', 'chr', 'addslashes', 'addcslashes',
+    'rand', 'date', 'strtotime', 'range', 'sprintf', 'levenshtein', 'str::upper', 'str::random', 'str::slug',
+    'str::snake', 'str::studly', 'str::length', 'str::ascii', 'str::singular', 'str::plural'
+];
--- controllers/modules/String.php
+++ controllers/modules/String.php
@@ -42,12 +42,10 @@ Command::register($trigger.'str2hex', function (Command $command) {
 });
 
 Message::listen('privmsg', function ($message) {
-    $whitelist = [
-        'md5', 'sha1', 'strlen', 'base64_decode', 'base64_encode', 'str_replace', 'number_format',
-        'ucwords', 'substr', 'strrev', 'str_shuffle', 'str_split', 'explode', 'chr', 'addslashes', 'addcslashes',
-        'rand', 'date', 'strtotime', 'range', 'sprintf', 'levenshtein', 'str::upper', 'str::random', 'str::slug',
-        'str::snake', 'str::studly', 'str::length', 'str::ascii', 'str::singular', 'str::plural'
-    ];
+    $whitelist = \Config::get('taylor::php', []);
+    if (!count($whitelist)) {
+        return;
+    }
 
     // make sure we have some params
     if (count($message->params) == 2) {
